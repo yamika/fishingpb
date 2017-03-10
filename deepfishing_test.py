01f0cd5b1b90e983aa9e6968d8586f0683d7ec4a
yamika <yamikasama@yahoo.co.jp>
2017-03-10 18:59:56 +0900

    change code

--- deepfishing_test.py
+++ deepfishing_test.py
@@ -15,7 +15,7 @@ import time
 
 tf.reset_default_graph() # 再実行時にグラフをクリア
 
-CKPTDIR = "/home/pi/ckptdir" # ディレクトリはあらかじめ作っておく
+CKPTDIR = "/home/pi/fishingpb/ckptdir-test" # ディレクトリはあらかじめ作っておく
 
 sess = tf.InteractiveSession()
 
@@ -31,19 +31,51 @@ def get_datas(datas):
         datas.append(data)
 
 def isHit(datas):
+    noHit = 0;
     Hit = 0;
+    for k in range(0,10):
 #         print datas[k][0]
-    if(do_test(datas)):
-        print "hit!?"
-        Hit += 1
+        if(do_test(datas[k][0])):
+            print "hit!?"
+            Hit += 1
+        else: noHit += 1
 
-    if(Hit):
+    if(Hit >= 4):
         return True
     else:
         return False
+
+NUM_CLASSES = 2 #  4クラス分類
+NUM_STEPS = 1100 #  学習回数
+LEN_SEQ = 10 # 系列長
+SIZE_INPUT = 3 # 入力データ数
+NUM_DATA = 890  # データ数
+NUM_TEST = 100 # テスト用のデータ数
+SIZE_BATCH = 3 # バッチサイズ
+NUM_NODE = 1024  # ノード数
+LEARNING_RATE = 0.01  # 学習率
+
+x = tf.placeholder(tf.float32, [None, LEN_SEQ, SIZE_INPUT])
+t = tf.placeholder(tf.int32, [None, 1])
+t_on_hot = tf.one_hot(t, depth=NUM_CLASSES, dtype=tf.float32)
+x_transpose = tf.transpose(x, [1, 0, 2])
+x_reshape = tf.reshape(x_transpose, [-1, 1])
+x_split = tf.split(x_reshape, LEN_SEQ, 0)
+lstm_cell = rnn.BasicLSTMCell(NUM_NODE, forget_bias=1.0)
+outputs, states = rnn.static_rnn(lstm_cell, x_split, dtype=tf.float32)
+w = tf.Variable(tf.random_normal([NUM_NODE, NUM_CLASSES]))
+b = tf.Variable(tf.random_normal([NUM_CLASSES]))
+logits = tf.matmul(outputs[-1], w) + b
+pred = tf.nn.softmax(logits)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=t_on_hot, logits=logits)
+loss = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
+train_step = optimizer.minimize(loss)
+correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(t,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 fishing_dic = {0: 'Failed', 1: 'Hit'}
 
-saver = tf.train.Saver()
+#saver = tf.train.Saver()
 
 frommodel = False
 
@@ -61,13 +93,34 @@ else:
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
 
+
+def get_labels(dataset):
+    """ラベル(正解データ)を1ofKベクトルに変換する"""
+    raw_labels = [item[12] for item in dataset]
+    labels = []
+    for l in raw_labels:
+        if l == 1:
+            labels.append([1])
+        elif l == 0:
+            labels.append([0])
+    return np.array(labels)
+
+def get_data(dataset):
+    """データセットをnparrayに変換する"""
+    raw_data = [list(item)[3:6] for item in dataset]
+    return np.array(raw_data)
+
+def conv_shape(seq):
+    target = [np.array(seq).reshape(-1,1)]
+    return np.array(target)
 def do_test(param):
+#     sess = tf.Session()
+#     sess.run(tf.global_variables_initializer())
     seq = param
     start = time.time()
 #     print("start : "+str(start))
 #     print(conv_shape(seq))
     ans = sess.run(pred, feed_dict={x: conv_shape(seq)})[0]
-    print ans
 #     print("end : "+str(start - time.time()))
     ans = np.array(ans)
     #print ans
@@ -85,10 +138,6 @@ def fin():
     sys.exit() #Exit
 
 #============================
-
-# LSTM_learning(dataset)
-
-# while True: pass
 #Analog IO init
 bus = smbus.SMBus(1)
 
@@ -104,7 +153,8 @@ try:
         time.sleep(0.1)
 
         if(len(datas) == 10):
-            params = convert(datas)
+            data = np.array(datas)
+            params = data[:,0:3]
             #print params
             ans = isHit(params)
 #             ans = False
@@ -126,15 +176,17 @@ try:
 #                         time.sleep(0.2)
 #                         #down()
 #                         break;
-
-                ans = False
             else:
                 print "Failed"
+                ans = False
 
             datas.pop()
         i += 1
 #         print i
         flag = True
+        if(i == 30 and flag):
+#             syakuri()
+            i = 0
 except KeyboardInterrupt:
     sess.close()
     fin()
